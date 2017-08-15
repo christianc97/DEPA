7c31b6e88a8a652de84e5abcf6430253ff5c7fd7
Christian <cristian_9719@hotmail.com>
2017-08-15 15:36:15 -0500

    se pusieron puntos en el track y se arreglo el blade

--- app/Http/Controllers/trackMensajeroController.php
+++ app/Http/Controllers/trackMensajeroController.php
@@ -11,55 +11,63 @@ use DB;
 
 class trackMensajeroController extends Controller {
 
-    protected $id=8;
+    protected $id = 8;
+
     public function index() {
-        $user=Auth::user()->id;
-        $tienePermiso=$this->validarPermisos($this->id, $user);
+        $user = Auth::user()->id;
+        $tienePermiso = $this->validarPermisos($this->id, $user);
         if ($tienePermiso) {
             return view('reportes.trackMensajero');
-        }else{
+        } else {
             return view('home');
         }
-        
     }
-    public  function show($id_mensajero){
+
+    public function show($id_mensajero) {
         $id_mensajero = $id_mensajero;
 
         $id_resource = DB::select('select t.id_resource from task t where t.id = ' . $id_mensajero . '');
         $id_resource = $id_resource[0]->id_resource;
-        
+
         $fecha_asignacion = DB::select('select th.datecreate from task_history th where th.task_id = ' . $id_mensajero . ' and th.type_task_status_id = 3 order by id desc limit 1');
         $fecha_asignacion = $fecha_asignacion[0]->datecreate;
-        
+
         $fecha_finalizacion = DB::select('select th.datecreate from task_history th where th.task_id = ' . $id_mensajero . ' and th.type_task_status_id = 5 order by id desc limit 1');
         if (empty($fecha_finalizacion)) {
-            $fecha_finalizacion= date('Y-m-d H:i:s');
-        }else{
+            $fecha_finalizacion = date('Y-m-d H:i:s');
+        } else {
             $fecha_finalizacion = $fecha_finalizacion[0]->datecreate;
-        }      
+        }
         $track = DB::select('SELECT * FROM track WHERE datecreate BETWEEN "' . $fecha_asignacion . '" AND "' . $fecha_finalizacion . '" AND tbl_users_id = ' . $id_resource . '');
 
-        return view("reportes/trackMensajero",["track"=>$track, "id_m"=>$id_mensajero]);
+        return view("reportes/trackMensajero", ["track" => $track, "id_m" => $id_mensajero]);
     }
 
     public function track(ReportesFormRequest $request) {
+
         $id_mensajero = $request->get('id_mensajero');
 
-        $id_resource = DB::select('select t.id_resource from task t where t.id = ' . $id_mensajero . '');
-        $id_resource = $id_resource[0]->id_resource;
-        
-        $fecha_asignacion = DB::select('select th.datecreate from task_history th where th.task_id = ' . $id_mensajero . ' and th.type_task_status_id = 3 order by id desc limit 1');
-        $fecha_asignacion = $fecha_asignacion[0]->datecreate;
-        
-        $fecha_finalizacion = DB::select('select th.datecreate from task_history th where th.task_id = ' . $id_mensajero . ' and th.type_task_status_id = 5 order by id desc limit 1');
-        if (empty($fecha_finalizacion)) {
-            $fecha_finalizacion= date('Y-m-d H:i:s');
-        }else{
-            $fecha_finalizacion = $fecha_finalizacion[0]->datecreate;
-        }      
-        $track = DB::select('SELECT * FROM track WHERE datecreate BETWEEN "' . $fecha_asignacion . '" AND "' . $fecha_finalizacion . '" AND tbl_users_id = ' . $id_resource . '');
+        if ($id_mensajero != '') {
+            $paradas = DB::select('select t.tipo_task_places,t.lat,t.long, t.direccion from task_places t where t.task_id = '.$id_mensajero.'');
+            
+            $id_resource = DB::select('select t.id_resource from task t where t.id = ' . $id_mensajero . '');
+            $id_resource = $id_resource[0]->id_resource;
 
-        return view("reportes/trackMensajero",["track"=>$track]);
+            $fecha_asignacion = DB::select('select th.datecreate from task_history th where th.task_id = ' . $id_mensajero . ' and th.type_task_status_id = 3 order by id desc limit 1');
+            $fecha_asignacion = $fecha_asignacion[0]->datecreate;
+
+            $fecha_finalizacion = DB::select('select th.datecreate from task_history th where th.task_id = ' . $id_mensajero . ' and th.type_task_status_id = 5 order by id desc limit 1');
+            if (empty($fecha_finalizacion)) {
+                $fecha_finalizacion = date('Y-m-d H:i:s');
+            } else {
+                $fecha_finalizacion = $fecha_finalizacion[0]->datecreate;
+            }
+            $track = DB::select('SELECT * FROM track WHERE datecreate BETWEEN "' . $fecha_asignacion . '" AND "' . $fecha_finalizacion . '" AND tbl_users_id = ' . $id_resource . '');
+
+            return view("reportes/trackMensajero", ["track" => $track, "id_m" => $id_mensajero,"paradas"=>$paradas]);
+        } else {
+            return redirect()->back();
+        }
     }
 
 }
