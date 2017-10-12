301d1a00288b72f95e6d717155643e73fc0a2cf9
Danie Ricardo Iles <danieliles18@gmail.com>
2017-10-12 15:56:53 -0500

    domicilios puntos mapa clientes

--- resources/views/domiciliosUsuarios/index.blade.php
+++ resources/views/domiciliosUsuarios/index.blade.php
@@ -18,7 +18,7 @@ and open the template in the editor.
 }
 
 .table-fixed tbody {
-  height: 500px;
+  height: 550px;
   overflow-y: auto;
   width: 100%;
 }
@@ -27,54 +27,39 @@ and open the template in the editor.
 .table-fixed tbody{
   display: block;
 }
-.table-fixed tfoot{
-  display: block;
-}
 
-.table-fixed thead > tr> th,
-.table-fixed tfoot > tr> td{
+.table-fixed thead > tr> th {
   float: left;
-  border-bottom-width: 0;
 }
 </style>
 
-    <div class="panel panel-default">
-      <div class="panel-body">
-        <table class="table table-fixed">
+<div class="container">                                                                                   
+  <div class="table-responsive">          
+  <table class="table table-fixed">
           <thead>
             <tr>
-              <th class="col-xs-1">Id</th>
-              <th class="col-xs-1">Empresa mu</th>
-              <th class="col-xs-2">Nombre empresa</th>
-              <th class="col-xs-2">Direcion</th>
-              <th class="col-xs-2">Username</th>
-              <th class="col-xs-2">Password</th>
-              <th class="col-xs-1">Ciudad</th>
+              <th >Id</th>
+              <th class="col-md-1" style="width:10%">Empresa mu</th>
+              <th class="col-md-1" style="width:15%">Nombre empresa</th>
+              <th class="col-md-2" style="width:15%">Direcion</th>
+              <th class="col-md-2" style="width:13%">Username</th>
+              <th class="col-md-2" style="width:13%">Password</th>
+              <th class="col-md-2" style="width:13%">Ciudad</th>
             </tr>
           </thead>
           <tbody>
             @foreach ($domicilios_usuarios as $du)
             <tr>
-              <td class="col-xs-1">{{$du->id}}</td>
-              <td class="col-xs-1"><a href="http://admin.mensajerosurbanos.com/empresas/{{$du->empresa_mu}}"  target="_blank">{{$du->empresa_mu}}</a></td>
-              <td class="col-xs-2">{{$du->nombre_empresa}}</td>
+              <td >{{$du->id}}</td>
+              <td class="col-xs-2"><a href="http://admin.mensajerosurbanos.com/empresas/{{$du->empresa_mu}}"  target="_blank">{{$du->empresa_mu}}</a></td>
+              <td class="col-xs-3">{{$du->nombre_empresa}}</td>
               <td class="col-xs-2">{{$du->direccion}}</td>
               <td class="col-xs-2">{{$du->username}}</td>
               <td class="col-xs-2">{{$du->password_reset_token}}</td>
-              <td class="col-xs-1">{{$du->ciudad}}</td>
+              <td class="col-xs-3">{{$du->ciudad}}</td>
             </tr>
             @endforeach
           </tbody>
-          <tfoot>
-            <tr>
-              <td class="col-xs-2">Total 1</td>
-              <td class="col-xs-2">Total 2</td>
-              <td class="col-xs-2">Total 3</td>
-              <td class="col-xs-2">Total 4</td>
-              <td class="col-xs-2">Total 5</td>
-              <td class="col-xs-2">Total 6</td>
-            </tr>
-          </tfoot>
         </table>
       </div>
 
