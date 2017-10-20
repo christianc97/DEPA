5051b90964142aea63a8f21e3a09c820f1dc4358
Danie Ricardo Iles <danieliles18@gmail.com>
2017-10-20 12:26:50 -0500

    modal con tabla que muestra informacion para guardar en un json(json por hacer)

--- resources/views/domicilios/show.blade.php
+++ resources/views/domicilios/show.blade.php
@@ -195,7 +195,7 @@ and open the template in the editor.
                 <th>Direccion</th>
                 <th>Ciudad</th>
                 <th>Parking</th>
-                <th></th>
+                <th>Tiempos</th>
                 </thead>
                 <tbody>
                     @foreach($puntos_domicilios as $pd)
@@ -205,9 +205,9 @@ and open the template in the editor.
                         <td>{{$pd->direccion}}</td>
                         <td>{{$pd->ciudad}}</td>
                         <td>{{$pd->parking}}</td>
-                        <td><a href="" data-target="#modal-view-{{$pd->id}}" data-toggle="modal"><button class="btn btn-info"><i class="fa fa-eye" aria-hidden="true"></i></button></a></td>
+                        <td><a href="" data-target="#modal-view-{{$pd->id}}" data-toggle="modal"><button class="btn btn-primary"><span class="glyphicon glyphicon-time"></span></button></a></td>
                     </tr>
-                @endforeach
+                    @endforeach
                 </tbody>
             </table>
         </div>
@@ -312,100 +312,7 @@ and open the template in the editor.
     </script>
 @endforeach
 @endsection
-    <!-- <script>
-            function lunes(value)
-            {
-                if(value==false)
-                {
-                    document.getElementById("lunes1").disabled=false;
-                    document.getElementById("lunes2").disabled=false;
-                }else if(value==true){
-                    document.getElementById("lunes1").disabled=true;
-                    document.getElementById("lunes2").disabled=true;
-                }
-            }
-            function martes(value)
-            {
-                if(value==false)
-                {
-                    document.getElementById("martes1").disabled=false;
-                    document.getElementById("martes2").disabled=false;
-                }else if(value==true){
-                    document.getElementById("martes1").disabled=true;
-                    document.getElementById("martes2").disabled=true;
-                }
-            }
-            function miercoles(value)
-            {
-                if(value==false)
-                {
-                    document.getElementById("miercoles1").disabled=false;
-                    document.getElementById("miercoles2").disabled=false;
-                }else if(value==true){
-                    document.getElementById("miercoles1").disabled=true;
-                    document.getElementById("miercoles2").disabled=true;
-                }
-            }
-            function jueves(value)
-            {
-                if(value==false)
-                {
-                    document.getElementById("jueves1").disabled=false;
-                    document.getElementById("jueves2").disabled=false;
-                }else if(value==true){
-                    document.getElementById("jueves1").disabled=true;
-                    document.getElementById("jueves2").disabled=true;
-                }
-            }
-            function viernes(value)
-            {
-                if(value==false)
-                {
-                    document.getElementById("viernes1").disabled=false;
-                    document.getElementById("viernes2").disabled=false;
-                }else if(value==true){
-                    document.getElementById("viernes1").disabled=true;
-                    document.getElementById("viernes2").disabled=true;
-                }
-            }
-            function sabado(value)
-            {
-                if(value==false)
-                {
-                    document.getElementById("sabado1").disabled=false;
-                    document.getElementById("sabado2").disabled=false;
-                }else if(value==true){
-                    document.getElementById("sabado1").disabled=true;
-                    document.getElementById("sabado2").disabled=true;
-                }
-            }
-            function domingo(value)
-            {
-                if(value==false)
-                {
-                    document.getElementById("domingo1").disabled=false;
-                    document.getElementById("domingo2").disabled=false;
-                }else if(value==true){
-                    document.getElementById("domingo1").disabled=true;
-                    document.getElementById("domingo2").disabled=true;
-                }
-            }
-            function festivos(value)
-            {
-                if(value==false)
-                {
-                    document.getElementById("festivos1").disabled=false;
-                    document.getElementById("festivos2").disabled=false;
-                }else if(value==true){
-                    document.getElementById("festivos1").disabled=true;
-                    document.getElementById("festivos2").disabled=true;
-                }
-            }
-            function falert(value)
-            {
-                var val = document.getElementById('dialunes').alert(val);
-            }
-    </script>-->
+   
     
 <script src="{{asset('js/jQuery-2.1.4.min.js')}}"></script>
 <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
