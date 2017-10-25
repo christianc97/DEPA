d1be1e05a0196ac91e2961d52cb09efbad180e78
Danie Ricardo Iles <danieliles18@gmail.com>
2017-10-25 16:48:15 -0500

    modal tiempos puntos en dev

--- resources/views/domicilios/ModalView.blade.php
+++ resources/views/domicilios/ModalView.blade.php
@@ -8,7 +8,7 @@
         <div class="modal-body">
              {!! Form::open(array('url' => 'domicilios/tiempos', $pd->id ,'method'=>'POST','autocomplete'=>'off') ) !!}
                 {{Form::token()}}
-                <input type="hidden" value="{{$pd->id}}" name="id">
+                <input type="hidden"  value="{{$pd->id}}" name="id">
             <table class="table table-hover">
                 <tr>
                    <th>Dias</th>
@@ -18,61 +18,61 @@
                 </tr>
                 <tr>
                     <td>Lunes</td>
-                    <td><input type="checkbox" name="24" value="24" onchange="lunes(this.checked);"></td>
-                    <td><input  type="number" required="" id="lunes1" name="lunes1" class="form-control" placeholder="Hora de apertura" name="apertura"></td>
-                    <td><input  type="number" required="" id="lunes2" name="lunes2" class="form-control" placeholder="Hora de cierre" name="cierre"></td>
+                    <td><input type="checkbox" name="lunes24" id="lunes24"  value="24"  onchange="lunes(this.checked);"></td>
+                    <td><input type="number" step="any" required="" id="lunes1" name="lunes1" class="form-control" placeholder="Hora de apertura" name="apertura"></td>
+                    <td><input type="number" step="any" required="" id="lunes2" name="lunes2" class="form-control" placeholder="Hora de cierre" name="cierre"></td>
                 </tr>
                 <tr>
                     <td>Martes</td>
-                    <td><input type="checkbox" name="24" value="24" onchange="martes(this.checked);"></td>
-                    <td><input type="number" required="" id="martes1" class="form-control" placeholder="Hora de apertura" name="martes1"></td>
-                    <td><input type="number" required="" id="martes2" class="form-control" placeholder="Hora de cierre" name="martes2"></td>
+                    <td><input type="checkbox" name="martes24"  value="24"  onchange="martes(this.checked);"></td>
+                    <td><input type="number"   step="any" required="" id="martes1" class="form-control" placeholder="Hora de apertura" name="martes1"></td>
+                    <td><input type="number"   step="any" required="" id="martes2" class="form-control" placeholder="Hora de cierre" name="martes2"></td>
                 </tr>
                 <tr>
                     <td>Miercoles</td>
-                    <td><input type="checkbox" name="24" value="24" onchange="miercoles(this.checked);"></td>
-                    <td><input type="number" required="" id="miercoles1" class="form-control" placeholder="Hora de apertura" name="miercoles1"></td>
-                    <td><input type="number" required="" id="miercoles2" class="form-control" placeholder="Hora de cierre" name="miercoles2"></td>
+                    <td><input type="checkbox" name="miercoles24"  value="24"  onchange="miercoles(this.checked);"></td>
+                    <td><input type="number"   step="any" required="" id="miercoles1" class="form-control" placeholder="Hora de apertura" name="miercoles1"></td>
+                    <td><input type="number"   step="any" required="" id="miercoles2" class="form-control" placeholder="Hora de cierre" name="miercoles2"></td>
                 </tr>
                 <tr>
                     <td>Jueves</td>
-                    <td><input type="checkbox" name="24" value="24" onchange="jueves(this.checked);"></td>
-                    <td><input type="number" required="" id="jueves1" class="form-control" placeholder="Hora de apertura" name="jueves1"></td>
-                    <td><input type="number" required="" id="jueves2" class="form-control" placeholder="Hora de cierre" name="jueves2"></td>
+                    <td><input type="checkbox" name="jueves24"  value="24"  onchange="jueves(this.checked);"></td>
+                    <td><input type="number"   step="any" required="" id="jueves1" class="form-control" placeholder="Hora de apertura" name="jueves1"></td>
+                    <td><input type="number"   step="any" required="" id="jueves2" class="form-control" placeholder="Hora de cierre" name="jueves2"></td>
                 </tr>
                 <tr>
                     <td>Viernes</td>
-                    <td><input type="checkbox" name="24" value="24" onchange="viernes(this.checked);"></td>
-                    <td><input type="number" required="" id="viernes1" class="form-control" placeholder="Hora de apertura" name="viernes1"></td>
-                    <td><input type="number" required="" id="viernes2" class="form-control" placeholder="Hora de cierre" name="viernes2"></td>
+                    <td><input type="checkbox" name="viernes24"  value="24"  onchange="viernes(this.checked);"></td>
+                    <td><input type="number"   step="any" required="" id="viernes1" class="form-control" placeholder="Hora de apertura" name="viernes1"></td>
+                    <td><input type="number"   step="any" required="" id="viernes2" class="form-control" placeholder="Hora de cierre" name="viernes2"></td>
                 </tr>
                 <tr>
                     <td>Sabados</td>
-                    <td><input type="checkbox" name="24" value="24" onchange="sabado(this.checked);"></td>
-                    <td><input type="number" required="" id="sabado1" class="form-control" placeholder="Hora de apertura" name="sabado1"></td>
-                    <td><input type="number" required="" id="sabado2" class="form-control" placeholder="Hora de cierre" name="sabado2"></td>
+                    <td><input type="checkbox" name="sabados24"  value="24"  onchange="sabado(this.checked);"></td>
+                    <td><input type="number"   step="any" required="" id="sabado1" class="form-control" placeholder="Hora de apertura" name="sabado1"></td>
+                    <td><input type="number"   step="any" required="" id="sabado2" class="form-control" placeholder="Hora de cierre" name="sabado2"></td>
                 </tr>
                 <tr>
                     <td>Domingos</td>
-                    <td><input type="checkbox" name="24" value="24" onchange="domingo(this.checked);"></td>
-                    <td><input type="number" required="" id="domingo1" class="form-control" placeholder="Hora de apertura" name="domingo1"></td>
-                    <td><input type="number" required="" id="domingo2" class="form-control" placeholder="Hora de cierre" name="domingo2"></td>
+                    <td><input type="checkbox" name="domingos24"  value="24"  onchange="domingo(this.checked);"></td>
+                    <td><input type="number"   step="any" required="" id="domingo1" class="form-control" placeholder="Hora de apertura" name="domingo1"></td>
+                    <td><input type="number"   step="any" required="" id="domingo2" class="form-control" placeholder="Hora de cierre" name="domingo2"></td>
                 </tr>
                 <tr>
                     <td>Festivos</td>
-                    <td><input type="checkbox" name="24" value="24" onchange="festivos(this.checked);"></td>
-                    <td><input type="number" required="" id="festivos1" class="form-control" placeholder="Hora de apertura" name="festivos1"></td>
-                    <td><input type="number" required="" id="festivos2" class="form-control" placeholder="Hora de cierre" name="festivos2"></td>
+                    <td><input type="checkbox" name="festivos24"  value="24"  onchange="festivos(this.checked);"></td>
+                    <td><input type="number"   step="any" required="" id="festivos1" class="form-control" placeholder="Hora de apertura" name="festivos1"></td>
+                    <td><input type="number"   step="any" required="" id="festivos2" class="form-control" placeholder="Hora de cierre" name="festivos2"></td>
                 </tr>
             </table>
         </div>
         <div class="modal-footer">
+          <button type="reset" class="btn btn-warning" >Limpiar</button>
           <button type="button" class="btn btn-danger " data-dismiss="modal">Cerrar</button>
-          <button type="submit" class="btn btn-primary" >Guardar</button>
+          <button type="submit" class="btn btn-primary" onclick="validar();">Guardar</button>
         </div>        
         {!! Form::close() !!}
       </div>
     </div>
 </div>
-
 
--- resources/views/domicilios/show.blade.php
+++ resources/views/domicilios/show.blade.php
@@ -217,7 +217,11 @@ and open the template in the editor.
     @foreach($puntos_domicilios as $pd)
      @include('domicilios.ModalView')
     @endforeach
-    <script>
+    
+
+@endsection
+   @foreach($puntos_domicilios as $pd)
+   <script>
             function lunes(value)
             {
                 if(value==false)
@@ -311,9 +315,143 @@ and open the template in the editor.
                 var val = document.getElementById('dialunes').alert(val);
             }
     </script>
-@endsection
-   
-    
+
+<script>
+function validar() {
+    var a0, a1, a2, b1, b2, c1, c2, d1, d2, e1, e2, f1, f2, g1, g2, h1, h2 ;
+    //lunes
+    a0 = document.getElementById("lunes24"). value;
+    a1 = document.getElementById("lunes1"). value;
+    a2 = document.getElementById("lunes2"). value;
+    //martes
+    b1 = document.getElementById("martes1"). value;
+    b2 = document.getElementById("martes2"). value;
+    //miercoles
+    c1 = document.getElementById("miercoles1"). value;
+    c2 = document.getElementById("miercoles2"). value;
+    //jueves
+    d1 = document.getElementById("jueves1"). value;
+    d2 = document.getElementById("jueves2"). value;
+    //viernes
+    e1 = document.getElementById("viernes1"). value;
+    e2 = document.getElementById("viernes2"). value;
+    //sabados
+    f1 = document.getElementById("sabado1"). value;
+    f2 = document.getElementById("sabado2"). value;
+    //domingos
+    g1 = document.getElementById("domingo1"). value;
+    g2 = document.getElementById("domingo2"). value;
+    //festivos
+    h1 = document.getElementById("festivos1"). value;
+    h2 = document.getElementById("festivos2"). value;
+
+    //lunes
+    if (isNaN(a1) || a1 < 0 || a1 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("lunes1"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    if (isNaN(a2) || a2 < 0 || a2 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("lunes2"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    //martes
+    if (isNaN(b1) || b1 < 0 || b1 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("martes1"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    if (isNaN(b2) || b2 < 0 || b2 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("martes2"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    //miercoles
+    if (isNaN(c1) || c1 < 0 || c1 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("miercoles1"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    if (isNaN(c2) || c2 < 0 || c2 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("miercoles2"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    //jueves
+    if (isNaN(d1) || d1 < 0 || d1 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("jueves1"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    if (isNaN(d2) || d2 < 0 || d2 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("jueves2"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    //viernes
+    if (isNaN(e1) || e1 < 0 || e1 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("viernes1"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    if (isNaN(e2) || e2 < 0 || e2 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("viernes2"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    //sabados
+    if (isNaN(f1) || f1 < 0 || f1 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("sabado1"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    if (isNaN(f2) || f2 < 0 || f2 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("sabado2"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    //domingos
+    if (isNaN(g1) || g1 < 0 || g1 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("domingo1"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    if (isNaN(g2) || g2 < 0 || g2 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("domingo2"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    //festivos
+    if (isNaN(h1) || h1 < 0 || h1 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("festivos1"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+    if (isNaN(h2) || h2 < 0 || h2 < 24) {
+        text = "ok";
+    } else {
+        document.getElementById("festivos2"). value = "";
+        alert("Ingrese los valores estalecidos\nnumeros entre (0 y 23.99)");
+    }
+}
+</script>
+@endforeach 
 <script src="{{asset('js/jQuery-2.1.4.min.js')}}"></script>
 <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
 <script>
