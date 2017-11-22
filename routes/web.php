f9931f7894e32d1951ba83c7f87e6da97d6d7c2f
Danie Ricardo Iles <danieliles18@gmail.com>
2017-11-22 16:55:07 -0500

    auth para endpoints

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('api/serviciosvistos/{id}', function ($id) {
       left join recursos r on r.tbl_users_id = d.id_resource
       where d.id_status = 2 and  d.task_id = '.$id);
     return $vistos;    
-})->middleware('auth.basic.once');
+});
 
 Route::get('api/tiempospuntos/{id}', function ($id) {
   $puntos = DB::connection('mu_domicilios')->select('select p.nombre as name, p.direccion as address, p.direccion2 as address2, p.zone, p.scheduleLabel,  p.lat, p.long, p.schedule, p.cityId, p.empresa_id from puntos p where p.empresa_id in (select e.id from empresa e where e.mu_ref = '.$id.' )');
