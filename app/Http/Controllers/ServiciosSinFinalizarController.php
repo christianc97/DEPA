9fa827d7cb7ac8ebd81b6f7908282e49b3acbf74
Danie Ricardo Iles <danieliles18@gmail.com>
2017-11-30 17:21:22 -0500

    tabla task_type_status en vista... y boton eliminar en domicilios puntos

--- app/Http/Controllers/ServiciosSinFinalizarController.php
+++ app/Http/Controllers/ServiciosSinFinalizarController.php
@@ -17,36 +17,14 @@ class ServiciosSinFinalizarController extends Controller
     }
 
     public function index(){
-    	$servicios_sin_finalizar = DB::select('SELECT t.id,
-                                                t.uuid,
-                                                (SELECT tp.order_id FROM task_places tp WHERE task_id = t.id AND tipo_task_places = 1 LIMIT 1) AS num_orden,
-                                                     t.date_created,
-                                                     t.fecha_inicio,
-                                                     t.hora_inicio,
-                                                     ts.nombre AS estado, 
-                                                     t.solicitante AS id_solicitante,
-                                                     p.nombre AS nombre_solicitante,
-                                                     t.id_company,
-                                                     t.name_company,
-                                                     tt.nombre AS tipo_servicio,
-                                                     t.valor_total,
-                                                     pa.nombre AS tipo_de_pago,
-                                                c.nombre AS ciudad
-                                            FROM
-                                                task t
-                                            INNER JOIN tbl_users sol ON sol.id = t.solicitante
-                                            INNER JOIN tbl_profiles p ON p.user_id = t.solicitante
-                                            INNER JOIN type_task tt ON tt.id = t.type_task_id
-                                            LEFT JOIN empresas em ON em.id = t.id_company 
-                                            LEFT JOIN recursos re ON re.tbl_users_id = t.id_resource
-                                            LEFT JOIN type_task_pago pa ON pa.id = t.estado_pago
-                                            LEFT JOIN type_task_status ts ON ts.id = t.estado
-                                            LEFT JOIN ciudad c ON c.id = t.ciudad_id
-                                            WHERE  
-                                            t.estado in (1,2,3,4)
-                                            order by t.fecha_inicio;');
     	
-    	return view('reportes.serviciosSinFinalizar', ["servicios_sin_finalizar" => $servicios_sin_finalizar]);
+
+        $type_task_status = DB::select('select ts.nombre estado, count(t.id) cantidad  from task t 
+                                        LEFT JOIN type_task_status ts ON ts.id = t.estado
+                                        where t.estado in (1,2,3,4) 
+                                        group by t.estado;');
+    	
+    	return view('reportes.serviciosSinFinalizar', ["type_task_status" => $type_task_status]);
     }
     
 }
